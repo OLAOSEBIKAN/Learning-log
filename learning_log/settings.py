4da1aed87a7154e10bf38c2266e6c263d8c1caba
olaitan <olaonipekunolaitan@gmail.com>
2020-04-29 04:52:23 +0100

    getting ready for deployment to heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^d$j_i(zp99(1or*xr878&wa5i4a--s#ck30g4qym@&sz$lu%5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,4 +128,18 @@ BOOTSTRAP3 = {
     'include_jquery': True,
     }
 
-
+# Heroku settings
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+           'default': dj_database_url.config(default='postgres://localhost')
+    }
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # Allow all host headers.
+    ALLOWED_HOSTS = ['*']
+       # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
